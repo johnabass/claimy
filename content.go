b8f5d9a52b5543bb48be4b480306f5a6b0f2f168
johnabass <johnbass1017@yahoo.com>
2023-03-21 17:43:49 -0700

    use a go template for swagger.yaml so we can inject runtime values for defaults

--- content.go
+++ content.go
@@ -1,17 +1,21 @@
 package main
 
 import (
+	"bytes"
 	"embed"
 	"net/http"
 
+	"text/template"
+
+	"github.com/lestrrat-go/jwx/v2/jwk"
 	"go.uber.org/fx"
 )
 
 //go:embed swaggerui
 var swaggerui embed.FS
 
-//go:embed swagger.yaml
-var swaggerYAML []byte
+//go:embed swagger.yaml.gt
+var swaggerYAML string
 
 type SwaggerUIHandler http.Handler
 type SwaggerYAMLHandler http.Handler
@@ -24,14 +28,36 @@ func provideContent() fx.Option {
 					http.FS(swaggerui),
 				)
 			},
-			func() SwaggerYAMLHandler {
-				return SwaggerYAMLHandler(
-					http.HandlerFunc(func(response http.ResponseWriter, _ *http.Request) {
-						// NOTE: don't set a content type, since that seems to mess up browsers
-						// since there is, as yet, no official MIME type for YAML
-						response.Write(swaggerYAML)
-					}),
+			func(key jwk.Key) (syh SwaggerYAMLHandler, err error) {
+				var (
+					tmplt *template.Template
+					yaml  []byte
 				)
+
+				tmplt, err = template.New("swagger.yaml").Parse(swaggerYAML)
+				if err == nil {
+					var output bytes.Buffer
+					err = tmplt.Execute(
+						&output,
+						map[string]any{
+							"defaultKeyID": key.KeyID(),
+						},
+					)
+
+					yaml = output.Bytes()
+				}
+
+				if err == nil {
+					syh = SwaggerYAMLHandler(
+						http.HandlerFunc(func(response http.ResponseWriter, _ *http.Request) {
+							// NOTE: don't set a content type, since that seems to mess up browsers
+							// since there is, as yet, no official MIME type for YAML
+							response.Write(yaml)
+						}),
+					)
+				}
+
+				return
 			},
 		),
 	)
